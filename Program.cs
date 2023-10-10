c9fb898e0ad5e0bb1cc668ad563441ddd01d870b
vaishakh <vaisakh.k@gds.ey.com>
2023-10-10 15:48:58 +0530

    added new custom query

--- Program.cs
+++ Program.cs
@@ -6,8 +6,16 @@ namespace sample1
         {
             Console.WriteLine("Hello, World!");
             String word = FindWord("hello");
+            int result = myObject.Add(5, 10);
             String word2 = "notused";
             Console.WriteLine(word);
+            Console.WriteLine(result);
+        }
+
+        public int Add(int a, int b)
+        {
+            // The 'a' parameter is not used within this method.
+            return b;
         }
 
         public static String FindWord(String word){
